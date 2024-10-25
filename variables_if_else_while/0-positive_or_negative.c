70218a173ce4ab381a9359cca51048d971acc723
lyamia <9849@holbertonstudents.com>
2024-10-25 13:19:48 +0000

    betty check

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/*
+ * main - Entry point of the program
+ *
+ * This program generates a random number and
+ * checks if the number is positive, negative, or zero.
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	/* your code goes here */
 
-	if (n > 0) 
-	{ 
+	if (n > 0)
+	{
 		printf("%d is positive\n", n);
-	} else if (n == 0) 
+	}
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	} else 
+	}
+	else
 	{
 		printf("%d is negative\n", n);
 	}
