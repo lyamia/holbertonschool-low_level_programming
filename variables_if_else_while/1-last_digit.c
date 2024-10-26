a26946bbda70309f1326f3035ab33bea316c54bc
lyamia <9849@holbertonstudents.com>
2024-10-26 13:23:59 +0000

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /**
  * main - - Determine the last digit of a random number and classify it.
@@ -15,23 +14,22 @@
 int main(void)
 {
 	int n;
-	int ld;
+	int last_d;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	 ld = n % 10;
-	if (ld > 5)
+	last_d = n % 10;
+	if (last_d > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
 	}
-	else if (ld == 0)
+	else if (last_d == 0)
 	{
-		printf("The last digit of %d is %d and is 0\n", n, ld);
+		printf("The last digit of %d is %d and is 0\n", n, last_d);
 	}
 	else
 	{
-		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
 	}
 	return (0);
 }
