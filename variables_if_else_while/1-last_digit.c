acac0adc021acf81bc4f42bb95fac2d71fc07bab
lyamia <9849@holbertonstudents.com>
2024-10-26 13:10:29 +0000

    The last digit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/*
+ * betty style doc for function main goes there
+ * main - - Determine the last digit of a random number and classify it.
+ *
+ * Description: This program assigns a random number to `n` each time it is
+ * executed, then determines the last digit of `n`. Based on the last digit,
+ * it prints whether it is greater than 5, equal to 0, or less than 6 and not 0.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int n;
+	int ld;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	 ld = n % 10;
+	if (ld > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+	}
+	else if (ld == 0)
+	{
+		printf("The last digit of %d is %d and is 0\n", n, ld);
+	}
+	else
+	{
+		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+	}
+	return (0);
+}
