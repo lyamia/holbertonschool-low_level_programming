f4cfe1d42dafb930f709e38d0c7bb19ecdb7095b
lyamia <9849@holbertonstudents.com>
2024-10-27 19:52:01 +0000

    add

---
+++ functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - entry point for testing _islower
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _islower('H');  // Test call to _islower with character 'H'
+    return (0);
+}
