f294e5f74d9168e1388ad6a36f0a900224ed7f05
lyamia <9849@holbertonstudents.com>
2024-10-28 10:24:55 +0000

    add files

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isalpha - main
+ * @c: var 1
+ *
+ * Return: zero
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
