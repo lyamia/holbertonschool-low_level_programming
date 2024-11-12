eea7528ca9d60a6e78a17050d4a69e2d10e3e058
lyamia <9849@holbertonstudents.com>
2024-11-12 11:23:08 +0000

    fix

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,6 +8,8 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	char *start = dest;
+
 	while (*dest != '\0')
 	{
 		dest++;
@@ -20,5 +22,5 @@ char *_strcat(char *dest, char *src)
 		dest++;
 	}
 	*dest = '\0';
-	return (dest);
+	return (start);
 }
