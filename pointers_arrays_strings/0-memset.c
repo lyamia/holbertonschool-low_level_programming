3a3aa7e3c88d1e522f73f82f96dd7353d5b33daa
lyamia <9849@holbertonstudents.com>
2024-11-13 12:12:08 +0000

    fix the style

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
- *
+ * _memset - fills memeory with a consonnat byte
+ * @s: pointer to the memory to be filled
+ * @b: byte to fill
+ * @n: nuber of bytes
  *
  */
 char *_memset(char *s, char b, unsigned int n)
