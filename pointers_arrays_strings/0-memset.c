c8c76a4ccacd7d38a62bcf310f256a489cdfdfea
lyamia <9849@holbertonstudents.com>
2024-11-13 12:14:25 +0000

    add return description

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * _memset - fills memeory with a consonnat byte
+ * _memset - fills memory with a consonant byte
  * @s: pointer to the memory to be filled
  * @b: byte to fill
  * @n: nuber of bytes
  *
+ * Return: pointer tot he array
  */
 char *_memset(char *s, char b, unsigned int n)
 {
