87e1d01a4a25396b1bae5cbb4bf007021de2d57a
lyamia <9849@holbertonstudents.com>
2024-11-25 13:03:26 +0000

    dd he main file

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * main -  Prints the name of the program followed by a newline
+ * @argc: THe number of command line args
+ * @argv: An array of comman dline args
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", arg[0]);
+return (0);
+}
